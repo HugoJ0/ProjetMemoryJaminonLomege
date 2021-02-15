122984a03e30e093f77045ed4eaf9c2111453cf4
HugoJ0 <hugoj0107@gmail.com>
2021-02-15 09:15:38 +0100

    Creation de fichier

---
+++ app/src/main/java/fr/iut/projetmemoryjaminonlomege/Niveau1.java
@@ -0,0 +1,14 @@
+package fr.iut.projetmemoryjaminonlomege;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Niveau1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niveau1);
+    }
+}
