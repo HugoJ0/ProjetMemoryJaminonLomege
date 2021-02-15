227d39246c70356c0e221574f219d5cbadb5414c
HugoJ0 <hugoj0107@gmail.com>
2021-02-15 09:37:05 +0100

    Ajout regles et debut main

---
+++ app/src/main/java/fr/iut/projetmemoryjaminonlomege/Regles.java
@@ -0,0 +1,14 @@
+package fr.iut.projetmemoryjaminonlomege;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Regles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regles);
+    }
+}
