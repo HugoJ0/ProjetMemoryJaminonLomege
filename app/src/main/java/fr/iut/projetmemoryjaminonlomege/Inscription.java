2486342d62219702a027050d6f0c683f54afcc32
whozghiar <theolomege@hotmail.fr>
2021-03-20 17:26:16 +0100

    Base de données + Insertions pour l'inscription

--- app/src/main/java/fr/iut/projetmemoryjaminonlomege/Inscription.java
+++ app/src/main/java/fr/iut/projetmemoryjaminonlomege/Inscription.java
@@ -6,15 +6,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class Inscription extends AppCompatActivity {
 
     Button retour;
+    Button inscription;
+    SQLiteHelper db;
+    EditText FirstName;
+    EditText LastName;
+    EditText mail;
+    EditText password;
+    RadioButton genre;
+    EditText dateNais;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+        mail = findViewById(R.id.id_input_mail_inscription);
+        password = findViewById(R.id.id_input_mdp_inscription);
+        FirstName = findViewById(R.id.id_input_nom);
+        LastName = findViewById(R.id.id_input_prenom);
+        genre = findViewById(R.id.id_radiobouton_genre);
+        dateNais = findViewById(R.id.id_input_datenaissance);
+        inscription = findViewById(R.id.id_bouton_inscription);
+
+        db = new SQLiteHelper(getApplicationContext());
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (db.addJoueur(mail.getText().toString(),password.getText().toString(),FirstName.getText().toString(),LastName.getText().toString(),genre.getText().toString(),dateNais.getText().toString(),0)){
+                    Intent intent = new Intent (Inscription.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(Inscription.this,"Erreur lors de l'inscription !", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
         retour=findViewById(R.id.inscription_id_bouton_retour);
         retour.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/fr/iut/projetmemoryjaminonlomege/SQLiteHelper.java
@@ -0,0 +1,75 @@
+package fr.iut.projetmemoryjaminonlomege;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.sql.Date;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    public static String BDD_NAME= "ColorMemoryBDD", TABLE_NAME ="Joueur",ID="id";
+    public static String MAIL = "mail", PASSWORD="password",FIRSTNAME = "prenom", LASTNAME = "nom", GENRE = "genre", DATE_NAISSANCE = "dateNais", SCORE = "score";
+
+
+    public SQLiteHelper(@Nullable Context context) {
+        super(context, BDD_NAME,null,1 );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql =
+                    "CREATE TABLE " + TABLE_NAME + " (" +
+                            ID+" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            MAIL + " TEXT," +
+                            PASSWORD + " TEXT," +
+                            FIRSTNAME + " TEXT," +
+                            LASTNAME + " TEXT," +
+                            GENRE + " TEXT," +
+                            DATE_NAISSANCE + " DATE," +
+                            SCORE + " INTEGER)";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String sql= "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+    boolean addJoueur(String mail, String password, String prenom, String nom, String genre, String dateNais, int score){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues data = new ContentValues();
+        data.put(MAIL,mail);
+        data.put(PASSWORD,password);
+        data.put(FIRSTNAME,prenom);
+        data.put(LASTNAME,nom);
+        data.put(GENRE,genre);
+        data.put(DATE_NAISSANCE, dateNais);
+        data.put(SCORE,score);
+
+        long result = db.insert(TABLE_NAME,null,data);
+        if (result != -1){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    boolean connection(String mail, String password){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql="SELECT * FROM Joueur WHERE mail='"+mail+"' AND password='"+password+"'";
+        Cursor cursor = db.rawQuery(sql,null);
+        if (cursor.moveToFirst()){
+            return true;
+        }
+        else
+            return false;
+    }
+}
