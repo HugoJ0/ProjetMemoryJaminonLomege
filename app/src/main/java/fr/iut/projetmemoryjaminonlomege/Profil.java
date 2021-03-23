43bed16666ae2f7c6a64537f8a2f3b40540ce03f
HugoJ0 <hugoj0107@gmail.com>
2021-03-23 21:25:13 +0100

    interface profil

--- app/src/main/java/fr/iut/projetmemoryjaminonlomege/Profil.java
+++ app/src/main/java/fr/iut/projetmemoryjaminonlomege/Profil.java
@@ -2,13 +2,28 @@ package fr.iut.projetmemoryjaminonlomege;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profil extends AppCompatActivity {
+
+    Button retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        retour=findViewById(R.id.id_bouton_retour);
+
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profil.this, Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
