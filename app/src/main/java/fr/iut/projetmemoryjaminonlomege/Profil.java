29b79e4a7e344d927a3f0b083b50371262db4261
whozghiar <theolomege@hotmail.fr>
2021-03-28 16:09:26 +0200

    Documentation Profil

--- app/src/main/java/fr/iut/projetmemoryjaminonlomege/Profil.java
+++ app/src/main/java/fr/iut/projetmemoryjaminonlomege/Profil.java
@@ -32,28 +32,43 @@ public class Profil extends AppCompatActivity {
         Bundle bundle2=intent2.getExtras();
         mail = bundle2.getString("mail");
         db = new SQLiteHelper(getApplicationContext());
-
+        /*
+            On récupère l'id du TextView du pseudo, pour y afficher le résultat de la fonction affPseudo() (voir class SQLiteHelper).
+         */
         pseudo = (TextView)findViewById(R.id.id_textview_pseudo);
         pseudo.setText(db.affPseudo(mail));
-
+        /*
+            On récupère l'id du TextView du nom, pour y afficher le résultat de la fonction affNom() (voir class SQLiteHelper).
+         */
         nom = (TextView)findViewById(R.id.id_textview_nom);
         nom.setText(db.affNom(mail));
-
+        /*
+            On récupère l'id du TextView du prénom, pour y afficher le résultat de la fonction affPrenom() (voir class SQLiteHelper).
+         */
         prenom = (TextView)findViewById(R.id.id_textview_prenom);
         prenom.setText(db.affPrenom(mail));
-
+        /*
+            On récupère l'id du TextView du mail, pour y afficher le mail du joueur (voir Bundle).
+         */
         email =(TextView)findViewById(R.id.id_textview_email);
         email.setText(mail);
-
+        /*
+            On récupère l'id du TextView du genre, pour y afficher le résultat de la fonction affGenre() (voir class SQLiteHelper).
+         */
         genre = (TextView)findViewById(R.id.id_textview_sexe);
         genre.setText(db.affGenre(mail));
-
+        /*
+            On récupère l'id du TextView de la date de naissance, pour y afficher le résultat de la fonction affDatenais() (voir class SQLiteHelper).
+         */
         dateNais = (TextView)findViewById(R.id.id_textview_datenaissance);
         dateNais.setText(db.affDateNais(mail));
 
 
         retour=findViewById(R.id.id_bouton_retour);
 
+        /*
+            Au clic sur le bouton retour, on renvoie l'utilisateur sur l'accueil du jeu.
+         */
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
